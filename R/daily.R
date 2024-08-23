c9e41f6bef36fd0857618d12383a1aa1ae40a42b
emilio-berti <emilio.berti90@gmail.com>
2024-08-23 11:55:18 +0200

    first commit"

---
+++ R/daily.R
@@ -0,0 +1,126 @@
+#' @title Request to GET daily summaries
+#'
+#' @export
+#' 
+#' @param station_id Character vector of station id(s).
+#' @param start_date Start date as character.
+#' @param end_date Start date as character.
+#'
+#' @details dates should be given in `YYYY-mm-dd` format.
+#' 
+#' @return A tibble with the stations within the `roi`.
+#' 
+.daily_request <- function(station_id, start_date, end_date) {
+  if (nchar(strsplit(start_date, "-")[[1]][1]) < 4) {
+    warning("   date format should be YYYY-MM-DD")
+  }
+  if (as.numeric(strsplit(start_date, "-")[[1]][2]) > 12) {
+    warning("   date format should be YYYY-MM-DD")
+  }
+  if (as.numeric(strsplit(start_date, "-")[[1]][3]) > 31) {
+    warning("   date format should be YYYY-MM-DD")
+  }
+  if (nchar(strsplit(end_date, "-")[[1]][1]) < 4) {
+    warning("   date format should be YYYY-MM-DD")
+  }
+  if (as.numeric(strsplit(end_date, "-")[[1]][2]) > 12) {
+    warning("   date format should be YYYY-MM-DD")
+  }
+  if (as.numeric(strsplit(end_date, "-")[[1]][3]) > 31) {
+    warning("   date format should be YYYY-MM-DD")
+  }
+
+  if (length(station_id) > 1) station_id <- paste(station_id, collapse = ",")
+  req <- paste0(
+    "https://www.ncei.noaa.gov/access/services/data/v1?",
+    "dataset=daily-summaries&",
+    "stations=", station_id, "&",
+    "startDate=", start_date, "&",
+    "endDate=", end_date, "&",
+    "includeAttributes=false&format=json"
+  )
+  return (req)
+}
+
+
+
+#' @title Download daily summaries
+#'
+#' @importFrom dplyr bind_rows mutate rename_with across group_by tally select
+#' @importFrom httr2 request req_perform resp_body_json
+#'
+#' @export
+#' 
+#' @param station_id Character vector of station id(s).
+#' @param start_date Start date as character.
+#' @param end_date Start date as character.
+#'
+#' @details dates should be given in `YYYY-mm-dd` format.
+#' 
+#' @return A tibble with the stations within the `roi`.
+#' 
+daily <- function(station_id, start_date, end_date) {
+  req <- .daily_request(station_id, start_date, end_date)
+  daily_data <- request(req)
+  daily_data <- req_perform(daily_data)
+  body <- resp_body_json(daily_data)
+  if (length(body) == 0) {
+    stop(" - No data found")
+  }
+
+  daily <- body |> bind_rows()
+  daily <- daily |> 
+    mutate(across(TMAX : (colnames(daily)[ncol(daily)]), ~as.numeric(.x))) |> 
+    rename_with(tolower) |> 
+    mutate(date = as.POSIXct(date))
+
+  class(daily) <- c("daily", class(daily))
+
+  return(daily)
+}
+
+#' @title Extract years from a POSIXct date.
+#'
+#' @export
+#'
+#' @param x POSIXct object.
+.years <- function(x) {
+  year <- format(as.Date(x, format="%Y/%m/%d"),"%Y")
+  return (year)
+}
+
+#' @title Calculate Coverage of daily summaries
+#'
+#' @importFrom dplyr mutate group_by tally ungroup select
+#' @importFrom tidyr drop_na pivot_longer pivot_wider
+#'
+#' @export
+#' 
+#' @param x Object of class `daily`.
+#'
+#' @details dates should be given in `YYYY-mm-dd` format.
+#' 
+#' @return A tibble with the stations within the `roi`.
+#' 
+daily_coverage <- function(x) {
+  stopifnot(is(x, "daily"))
+  coverage <- x |> 
+    mutate(
+      year = .years(date),
+      n_years = max(as.numeric(year)) - min(as.numeric(year)) + 2
+    ) |> 
+    pivot_longer(cols = tmax : (colnames(x)[ncol(x)])) |> 
+    drop_na() |> 
+    group_by(station, name, n_years) |> 
+    tally() |> 
+    ungroup() |> 
+    mutate(coverage = n / 365 / n_years) |> 
+    select(station, name, coverage) |> 
+    mutate(coverage = ifelse(coverage > 1, 1, coverage)) |> 
+    pivot_wider(names_from = name, values_from = coverage) |> 
+    mutate(across(where(is.numeric), ~replace_na(.x, 0)))
+
+  return(coverage)
+}
+
+
---
+++ R/stations.R
@@ -0,0 +1,43 @@
+#' @title Get Stations for Area
+#' 
+#' @importFrom methods is
+#' @importFrom terra vect plot points
+#' @importFrom tibble as_tibble
+#' @importFrom utils read.table download.file
+#' 
+#' @export
+#' 
+#' @param roi SpatVector defining the region of interest.
+#' @param show Logical, show the stations on a map?
+#' 
+#' @return A tibble with the stations within the `roi`.
+#' 
+stations <- function(roi, show = FALSE) {
+  stopifnot(is(roi, "SpatVector"))
+  if (length(roi) > 1) roi <- aggregate(roi)
+
+  message(" - Downloading stations inventory")
+  inventory <- tempfile()
+  download.file(
+    "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt",
+    inventory
+  )
+  
+  stations <- read.table(
+    inventory,
+    header = FALSE,
+    col.names = c("id", "lat", "lon", "dataType", "start", "end")
+  ) |> 
+    as_tibble() |> 
+    vect(crs = "EPSG:4326")  
+
+  if (crs(stations) != crs(roi)) stations <- project(stations, roi)
+  s <- stations[relate(stations, roi, "within", pairs = FALSE)[, 1], ]
+
+  if (show) {
+    plot(roi, col = "grey90")
+    points(s, col = "grey20", bg = "dodgerblue", pch = 21, cex = 1.5)    
+  }
+
+  return(s)
+}
